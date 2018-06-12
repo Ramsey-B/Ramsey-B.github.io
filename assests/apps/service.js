c65f10b5f786a23471d181bf9c233aaf5fcffc64
Ramsey Bland <ramsey.bland@gmail.com>
2018-06-11 22:15:13 -0600

    styling

--- assests/apps/service.js
+++ assests/apps/service.js
@@ -1,5 +1,5 @@
 function Service() {
-  var lang = ["assests/pics/html.png", "assests/pics/css.logo.png", "assests/pics/javascript.png"]
+  var lang = ["assests/pics/html.png", "assests/pics/css.logo.png", "assests/pics/javascript.png", "assests/pics/csharp.png"]
   var frame = ["assests/pics/bootstrap.png", "assests/pics/vue.png", "assests/pics/node.png", "assests/pics/phaser.png"]
 
   this.languages = () => {
